8d6e0f4b06370005265734e378e67e2bb04f2729
Valentine <val.qwertynerd@gmail.com>
2026-03-01 13:21:58 +0100

    Add output formating in compile script.

--- tools/compile.bat
+++ tools/compile.bat
@@ -1,5 +1,10 @@
-del ..\qc\progs.pak
+echo off
+echo =============================================
+ECHO FTEQCC COMPILATION:
+del ..\qc\vqmdk.pak
 fteqcc-compiler\fteqcc64.exe -std=qcc -src ../qc -o ../qc/progs.dat
+
+echo =============================================
 copy ..\qc\progs.dat \quakepak
 cd quakepak\
-qpak64.exe p list_to_pak.txt ..\..\qc\progs.pak
+qpak64.exe p list_to_pak.txt ..\..\qc\vqmdk.pak
